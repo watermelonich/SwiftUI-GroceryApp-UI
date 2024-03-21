fda47f2223e32d00e099e8da078b1e9d026ed2ac
Nich <128910037+watermelonich@users.noreply.github.com>
2024-03-21 15:29:44 +0100

    Initial commit

---
+++ GrocApp/GroceryAppTutApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryAppTutApp.swift
+//  GroceryAppTut
+//
+//  Created by Nich on 15/01/24.
+//
+
+import SwiftUI
+
+@main
+struct GroceryAppTutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
